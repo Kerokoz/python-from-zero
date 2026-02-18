f13a962b13ef93efd565249aaa4ea305a5ba8b09
Kerokoz <kerokogaming@gmail.com>
2026-02-18 18:49:21 +0300

    age logic added

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 print ("Kerokoz to the world")
-name = input ("What is your name?")
-print (f"Nice to meet you,{name}")
+
+name = input ("What is your name? ")
+age = int(input("How old are you? "))
+
+print (f"Nice to meet you,{name}!")
+
+if age < 18:
+	print ("You are young and full of energy and potential")
+else:
+	print ("You are building wisdom and experience")
